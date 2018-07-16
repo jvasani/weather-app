8e432d52b99d0b8f6d86041c28bc751b9ab2886e
Jigar Vasani <jigarvasani@Jigars-MacBook-Pro.local>
2018-07-16 16:34:38 -0400

    Initial CSS Changes

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,11 +4,14 @@ class Titles extends React.Component{
 
     render(){
         return(
+            <div>
             <div className="title_1">
                 <h1>Weather Finder</h1>
-                <div className="title_subtitle">
-                    <p>Find out temprature and conditions and more..... </p>
-                </div>
+               
+            </div>
+            <div className="title_subtitle">
+                <p>Find out temprature and conditions and more..... </p>
+            </div>
             </div>
         );
     }
